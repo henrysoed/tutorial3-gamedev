215c5cfe9943458a850a9091b8e817bd31bf0ef2
Henrysoed <henry.soedibjo@gmail.com>
2025-03-14 19:10:05 +0700

    Tutorial 5 Latihan: Membuat Animasi dengan AnimatedSprite

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,104 +1,38 @@
 extends CharacterBody2D
 
-@export var gravity = 200.0
-@export var walk_speed = 200
-@export var jump_speed = -300
+@export var SPEED := 200
+@export var JUMP_SPEED := -400
+@export var GRAVITY := 1200
+@onready var animplayer = $AnimatedSprite2D
 
-# Double jump variables
-var max_jumps = 2
-var jump_count = 0
+const UP = Vector2(0,-1)
 
-# Dash variables
-var is_dashing = false
-var dash_duration = 0.2
-var dash_timer = 0.0
-var dash_cooldown = 1.0
-var dash_cooldown_timer = 0.0
-var can_dash = true
-var dash_speed_multiplier = 2.5
-var normal_speed = walk_speed
-var double_tap_threshold = 200
-var last_left_press_time = 0
-var last_right_press_time = 0
 
-# Crouch variables
-var is_crouching = false
-var crouch_speed = 100
+func _get_input():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_SPEED
 
-func _physics_process(delta):
-	velocity.y += delta * gravity
-	
-	# Double jump logic
-	if Input.is_action_just_pressed('ui_up'):
-		if is_on_floor():
-			velocity.y = jump_speed
-			jump_count = 1
+  # Get the input direction and handle the movement/deceleration.
+  # As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	var animation = "idle"
+	if direction:
+		animation = "walk right"
+		velocity.x = direction * SPEED
+		if direction>0:
+			animplayer.flip_h = false
 		else:
-			if jump_count < max_jumps:
-				velocity.y = jump_speed
-				jump_count += 1
-	
-	if is_on_floor():
-		jump_count = 0
-	
-	# Crouching logic
-	if Input.is_action_pressed("ui_down"):
-		if not is_crouching:
-			is_crouching = true
-			walk_speed = crouch_speed
-			$Sprite.scale.y = 0.5
+			animplayer.flip_h = true
 	else:
-		if is_crouching:
-			is_crouching = false
-			walk_speed = normal_speed
-			$Sprite.scale.y = 1.0
-	
-	# Dash cooldown
-	if not can_dash:
-		dash_cooldown_timer -= delta
-		if dash_cooldown_timer <= 0:
-			can_dash = true
-	
-	# Handle dash duration
-	if is_dashing:
-		dash_timer -= delta
-		if dash_timer <= 0:
-			is_dashing = false
-	
-	# Movement handling
-	if not is_dashing:
-		if Input.is_action_pressed("ui_left"):
-			velocity.x = -walk_speed
-		elif Input.is_action_pressed("ui_right"):
-			velocity.x = walk_speed
-		else:
-			velocity.x = 0
-	
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if animplayer.animation!=animation:
+		animplayer.play(animation)
+
 	move_and_slide()
 
-func _input(event):
-	# Double tap detection for dash
-	if event.is_action_pressed("ui_left"):
-		var current_time = Time.get_ticks_msec()
-		if current_time - last_left_press_time < double_tap_threshold:
-			start_dash(-1)
-		last_left_press_time = current_time
-	elif event.is_action_pressed("ui_right"):
-		var current_time = Time.get_ticks_msec()
-		if current_time - last_right_press_time < double_tap_threshold:
-			start_dash(1)
-		last_right_press_time = current_time
 
-func start_dash(direction):
-	if can_dash:
-		is_dashing = true
-		dash_timer = dash_duration
-		velocity.x = direction * normal_speed * dash_speed_multiplier
-		can_dash = false
-		dash_cooldown_timer = dash_cooldown
-
-func _ready():
-	normal_speed = walk_speed
-	
-func _process(delta):
-	pass
+func _physics_process(delta: float) -> void:
+	velocity.y += delta*GRAVITY
+	_get_input()
+	move_and_slide()
